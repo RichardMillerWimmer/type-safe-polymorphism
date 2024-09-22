6506b38919fb2fa1a2da612ac1da90725908ffd2
Richard Miller <richardmmill@gmail.com>
2024-09-22 15:18:28 -0400

    update tests

--- src/component/DynamicComponent.test.tsx
+++ src/component/DynamicComponent.test.tsx
@@ -5,33 +5,37 @@ import '@testing-library/jest-dom'
 
 describe('DynamicComponent', () => {
     describe('Dynamic Link', () => {
-        it('renders a link when "as" is set to "link"', () => {
-            render(<DynamicComponent as="a" href="/home">Home</DynamicComponent>)
+        it('renders a link when a href is passed', () => {
+            render(<DynamicComponent href="/home">Home</DynamicComponent>)
             expect(screen.getByRole('link', { name: 'Home' })).toBeVisible()
             expect(screen.getByRole('link')).toHaveAttribute('href', '/home')
         })
         it('renders a link with a custom class name', () => {
-            render(<DynamicComponent as="a" href="/home" className='className-prop'>Home</DynamicComponent>)
+            render(<DynamicComponent href="/home" className='className-prop'>Home</DynamicComponent>)
             expect(screen.getByRole('link', { name: 'Home' })).toHaveClass('className-prop')
         })
     });
     describe('Dynamic Button', () => {
         const mockOnClick = vi.fn()
-        it('renders a button when "as" is set to "button"', () => {
-            render(<DynamicComponent as="button" onClick={mockOnClick}>Click</DynamicComponent>)
+        it('renders a button when an onClick is passed', () => {
+            render(<DynamicComponent onClick={mockOnClick}>Click</DynamicComponent>)
             expect(screen.getByRole('button', { name: 'Click' })).toBeVisible()
             fireEvent.click(screen.getByRole('button'))
             expect(mockOnClick).toHaveBeenCalledTimes(1)
         })
         it('renders a button with a custom class name', () => {
-            render(<DynamicComponent as="button" onClick={mockOnClick} className='className-prop'>Click</DynamicComponent>)
+            render(<DynamicComponent onClick={mockOnClick} className='className-prop'>Click</DynamicComponent>)
             expect(screen.getByRole('button', { name: 'Click' })).toHaveClass('className-prop')
         })
     });
     it('returns null when conditions are not met', () => {
-        render(<DynamicComponent as="a">Home</DynamicComponent>)
+        render(<DynamicComponent>Home</DynamicComponent>)
         expect(screen.queryByRole('link', { name: 'Home' })).toBeNull()
-        render(<DynamicComponent as="button">Click</DynamicComponent>)
+        render(<DynamicComponent>Click</DynamicComponent>)
+        expect(screen.queryByRole('button', { name: 'Click' })).toBeNull()
+    })
+    it('returns null when "as" is not set', () => {
+        render(<DynamicComponent href='/'>Click</DynamicComponent>)
         expect(screen.queryByRole('button', { name: 'Click' })).toBeNull()
     })
 })
